87b62a677c3f19978068098f9ca13ad0a2e11a14
rjem201 <rjem201@exeter.ac.uk>
2020-03-07 18:50:54 +0000

    fixed creator authentication

--- routes/trackLanding.js
+++ routes/trackLanding.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('creator/start', { title: 'Express' });
+  if (req.session.currentUser == undefined || req.session.currentUser.role == 'Hunter') {
+    res.redirect('/login')
+  }
+  else {
+    res.render('creator/start', { title: 'Express' });
+    console.log(req.session);
+  }
+
 });
 
 
