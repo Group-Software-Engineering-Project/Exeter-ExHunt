264a0e2d0afa1c0fb278ec1c625c46921f33ed7a
rjem201 <rjem201@exeter.ac.uk>
2020-03-08 23:51:04 +0000

    testing javascripts
    trying to get javascript to pass the name of the track when clicked

---
+++ public/javascripts/hunter-click.js
@@ -0,0 +1,11 @@
+//const button = document.getElementsByClassName('track_div');
+
+//function clicked(){
+ //   console.log('button was clicked');
+//}
+
+
+//jQuery("classNameofDiv").click(function() {
+//    var contentPanelId = jQuery(this).attr("id");
+//    alert(contentPanelId);
+//});
