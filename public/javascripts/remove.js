4cd534799ab20ebb209ce8829371cb5af7f653cf
fhhindley99 <60666925+fhhindley99@users.noreply.github.com>
2020-03-08 22:17:36 +0000

    changes

--- public/javascripts/remove.js
+++ public/javascripts/remove.js
@@ -1,8 +1,7 @@
 console.log('Client-side code running');
 
 const button = document.getElementById('remove');
-const track_name = track.name;
 
 button.addEventListener('click', function(e) {
-  console.log('button was clicked and the name of the track is:' + track_name);
+  console.log('button was clicked');
 });
