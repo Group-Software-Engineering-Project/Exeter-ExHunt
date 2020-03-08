d4d381a7e9c4413625c4feede4b9bd456ca5aa18
rjem201 <rjem201@exeter.ac.uk>
2020-03-08 22:53:30 +0000

    finished basic hunter select

--- routes/hunters.js
+++ routes/hunters.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const Tracks = require('../models/tracks');
 
 
 router.get('/', function(req, res, next) {
-
-  res.render('hunters', {challenges:4});
+  Tracks.find({},"creator name number_of_challenges track_ranking",function(err,tracks){
+    console.log(tracks)
+    res.render('hunter/hunters', {challenges:2,tracks:tracks});
+  });
 });
 
 
