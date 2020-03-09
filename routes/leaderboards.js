b328941f164291795431022dd525a43bd1a85830
fhhindley99 <60666925+fhhindley99@users.noreply.github.com>
2020-03-09 16:01:53 +0000

    changes

--- routes/leaderboards.js
+++ routes/leaderboards.js
@@ -2,9 +2,20 @@ const express = require('express');
 const leaderboardsViewer = express.Router();
 const mongoose = require('mongoose');
 const Tracks = require('../models/tracks');
+const User = require("../models/user");
+var tracksTable;
+var usersTable;
 
 leaderboardsViewer.get('/', function(req, res, next) {
-    res.render('leaderboardsPage', { title: 'Leaderboards'})
+    Tracks.find({},"creator name number_of_challenges track_ranking",function(err,tracks){
+        console.log(tracks);
+        tracksTable = tracks;
+    })
+    User.find({},"username track_hunter_ranking",function(err,users){
+        console.log(users);
+        usersTable = users;
+    })
+    res.render('leaderboardsPage', { title: 'Leaderboards', tracksTable, usersTable});
 });
 
 module.exports = leaderboardsViewer;
