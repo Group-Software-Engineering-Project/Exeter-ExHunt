99d7c1a1011e9a70ee065ee1f0e509e4291f0455
fhhindley99 <60666925+fhhindley99@users.noreply.github.com>
2020-03-09 17:39:08 +0000

    Leaderboard Complete!

--- routes/leaderboards.js
+++ routes/leaderboards.js
@@ -3,19 +3,13 @@ const leaderboardsViewer = express.Router();
 const mongoose = require('mongoose');
 const Tracks = require('../models/tracks');
 const User = require("../models/user");
-var tracksTable;
-var usersTable;
 
 leaderboardsViewer.get('/', function(req, res, next) {
     Tracks.find({},"creator name track_ranking",function(err,tracks){
-        console.log(tracks);
-        tracksTable = tracks;
-    })
-    User.find({role:"Hunter"},"username track_hunter_ranking",function(err,users){
-        console.log(users);
-        usersTable = users;
-    })
-    res.render('leaderboardsPage', { title: 'Leaderboards', tracksTable, usersTable});
+        User.find({role:"Hunter"},"username track_hunter_ranking",function(err,users){
+            res.render('leaderboardsPage', { title: 'Leaderboards', tracks, users});
+        }).sort([['track_hunter_ranking', -1]]);
+    }).sort([['track_ranking', -1]]);
 });
 
 module.exports = leaderboardsViewer;
