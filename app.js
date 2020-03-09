06a7e5988632802fd205f62454e6e8cd72eb1a63
Lucas Martin Calderon <lucasmartincalderon1@gmail.com>
2020-03-09 16:52:33 +0000

    Branch revised and accepted

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const hunterRoutes = require("./routes/hunters");
 const creatorRoutes = require("./routes/trackLanding");
 const challengeLoopRoutes = require('./routes/trackCreatorloop');
 const trackViewerRoutes = require('./routes/trackViewer');
+const feedback = require('./routes/feedback')
 const createError = require('http-errors');
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
@@ -80,7 +81,8 @@ app.use('/', authRoutes);
 app.use('/hunters', hunterRoutes); 
 app.use('/creator', creatorRoutes);
 app.use('/challenge_loop',challengeLoopRoutes);
-app.use('/view_track',trackViewerRoutes);
+app.use('/view_track', trackViewerRoutes);
+app.use('/feedback', feedback);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/trackViewer.js
+++ routes/trackViewer.js
@@ -10,7 +10,7 @@ trackViewer.get('/', function(req, res, next) {
     if (req.session.currentUser == undefined || req.session.currentUser.role == 'Hunter') {
         res.redirect('/login')
     }
-    else{
+    else {
         const username = req.session.currentUser.username;
         Tracks.find({creator:username},"creator name number_of_challenges",function(err,tracks){
             console.log(tracks);
