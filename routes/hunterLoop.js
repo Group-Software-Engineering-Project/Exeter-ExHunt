f47938d9bd0b3771fddc526a889666a80f628da0
rjem201 <rjem201@exeter.ac.uk>
2020-03-10 20:25:22 +0000

    save track pos to user

--- routes/hunterLoop.js
+++ routes/hunterLoop.js
@@ -5,9 +5,10 @@ const GridFsStorage = require('multer-gridfs-storage');
 const grid = require('gridfs-stream');
 const Challenges = require('../models/challenges');
 const Tracks = require('../models/tracks');
+const User = require('../models/user')
 
 var currentTrack;
-var x = 0;
+var x;
 
 const conn = mongoose.createConnection('mongodb://localhost/exhunt');
 
@@ -18,33 +19,55 @@ conn.once('open', () => {
     gfs.collection('uploads');
 });
 
-//
-
 router.get('/',function(req,res){
+  User.findOne({username:req.session.currentUser.username},function(err,user){
+    if(user.track_name==req.session.hunter_track.name){
+      x=user.challenge_level;
+      console.log("continue");
+    }
+    else {
+      x=0;
+    }
+    res.redirect('/track_loop/loop');
+  });
+});
+
+
+router.get('/loop',function(req,res){
     currentTrack = req.session.hunter_track;
-    gfs.files.find({_id: { $in : [mongoose.Types.ObjectId(currentTrack.challenges[x].Vid1ID),mongoose.Types.ObjectId(currentTrack.challenges[x].Vid2ID)]}}).toArray((err,file)=>{
+      User.updateOne({username:req.session.currentUser.username},{track_name:currentTrack.name}).then(result=>{
+        console.log(result);
+      });
+      gfs.files.find({_id: { $in : [mongoose.Types.ObjectId(currentTrack.challenges[x].Vid1ID),mongoose.Types.ObjectId(currentTrack.challenges[x].Vid2ID)]}}).toArray((err,file)=>{
         // Check if files
         if (!file || file.length === 0) {
           res.render('hunter/hunter_loop',{})
+
         }
         else {
-            if (x==currentTrack.number_of_challenges) {
-
+          if (x==currentTrack.number_of_challenges) {
+            User.updateOne({username:req.session.currentUser.username},{track_name:"",challenge_level:0}).then(result=>{
               res.render('hunter/hunter_loop',{files:file,end:true});
-            }
-            else {
+            });
+            
+          }
+          else {
+            //console.log(file);
+            User.updateOne({username:req.session.currentUser.username},{challenge_level:x}).then(result=>{
               x++;
-              console.log(x);
+              console.log("x is "+x);
               res.render('hunter/hunter_loop',{files:file,end:false});
-            }
-            
-            
-        }
-      });
+            });
+          }
+      }
+    });
+
+
+    
 
 });
 
-router.get('/video/:filename', (req, res) => {
+router.get('/track_loop/video/:filename', (req, res) => {
     gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
       // Check if file
       if (!file || file.length === 0) {
@@ -53,7 +76,6 @@ router.get('/video/:filename', (req, res) => {
         });
       }
       // If File exists this will get executed
-      console.log("plese work");
       const readstream = gfs.createReadStream(file.filename);
       return readstream.pipe(res);
     });
